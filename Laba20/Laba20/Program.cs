5d074b5bbd47d045f40cc437c1229acd819bf301
war10cken <44408023+war10cken@users.noreply.github.com>
2021-10-26 11:31:00 +0300

    Update Program.cs

--- Laba20/Laba20/Program.cs
+++ Laba20/Laba20/Program.cs
@@ -64,8 +64,6 @@ namespace Laba20
             int count = 0, current, next, previous;
 
             List<Pair<int, int>> pairs = new();
-
-            // digits = new List<int> {1, 1, 2, 3, 4, 5, 6, 6, 7};
             
             for (int i = 1; i < digits.Count; i++)
             {
@@ -233,6 +231,13 @@ namespace Laba20
                 a.Add(new Point(_random.Value.Next(-10, 10), _random.Value.Next(-10, 10)));
             }
 
+            for (int i = 0; i < a.Count(); i++)
+            {
+                Console.WriteLine($"Точка {i + 1}");
+                Console.WriteLine($"Y: {a[i].Y}");
+                Console.WriteLine($"X: {a[i].X}");                
+            }
+            
             a = a.Where(point => point.X < 0)
                 .Where(point => point.Y > 0)
                 .OrderBy(point => point.X)
