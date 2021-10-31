7f26536ee115c16b3849ab5081768e53a8a091e7
war10cken <44408023+war10cken@users.noreply.github.com>
2021-10-31 14:18:49 +0300

    Done Laba 5

---
+++ Laba5/Laba5/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace Laba5
+{
+    public struct Point
+    {
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float X { get; }
+        public float Y { get; }
+    }
+}
